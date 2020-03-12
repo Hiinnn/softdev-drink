b38f1d42a541f65a57c1516dbb0aacfe271380d1
Hiinnn <hin_pansa@hotmail.com>
2020-03-12 21:49:27 +0700

    testbranch

---
+++ src/component/Hot-bars/Hot-bar.js
@@ -0,0 +1,8 @@
+import React, { Component } from 'react';
+import './Hot-bar.css';
+
+export default class HotBar extends Component{
+    return(
+        
+    );
+}
--- src/pages/User-home/User-home.js
+++ src/pages/User-home/User-home.js
@@ -1,6 +1,7 @@
 import React from 'react';
 import Navbar from '../../component/Nav-bar/Nav-bar';
 import Slider from '../../component/Slideshow/Slideshow';
+import './User-home.css';
 
 class UserHome extends React.Component{
     render() {
@@ -8,9 +9,15 @@ class UserHome extends React.Component{
             <div>
                 <Navbar/>
                 <Slider/>
-                <div className="main-content-container"></div>
+                <div className="main-content-container">
+                    <img src={require('../../asset/MainBackground/bg1cp.jpeg')}>
+                        
+                    </img>
+                </div>
             </div>
         );
     }
 }
 export default UserHome;
+
+eiei
