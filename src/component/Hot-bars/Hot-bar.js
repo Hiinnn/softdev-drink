8f4904cc54caaeae7768646c97a3ddc8531dc5d3
Hiinnn <hin_pansa@hotmail.com>
2020-03-13 15:33:20 +0700

    fix error version in vscode

--- src/component/Hot-bars/Hot-bar.js
+++ src/component/Hot-bars/Hot-bar.js
@@ -2,7 +2,5 @@ import React, { Component } from 'react';
 import './Hot-bar.css';
 
 export default class HotBar extends Component{
-    return(
-        
-    );
+    // return('eiei');
 }
--- src/pages/User-home/User-home.js
+++ src/pages/User-home/User-home.js
@@ -1,6 +1,7 @@
 import React from 'react';
 import Navbar from '../../component/Nav-bar/Nav-bar';
 import Slider from '../../component/Slideshow/Slideshow';
+import Hotbar from '../../component/Hot-bars/Hot-bar';
 import './User-home.css';
 
 class UserHome extends React.Component{
@@ -9,20 +10,11 @@ class UserHome extends React.Component{
             <div>
                 <Navbar/>
                 <Slider/>
-<<<<<<< HEAD
-                <div class="main-content-container">
-                    <img src={require("../../asset/HomeBG/bg1cp.jpg")}/>
-=======
                 <div className="main-content-container">
-                    <img src={require('../../asset/MainBackground/bg1cp.jpeg')}>
-                        
-                    </img>
->>>>>>> 7cf16304d67540d308d7f110df5a5b3ec4df8497
+                    {/* <Hotbar/> */}
                 </div>
             </div>
         );
     }
 }
 export default UserHome;
-
-eiei
