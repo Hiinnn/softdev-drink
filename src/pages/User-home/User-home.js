f733e33b3e476e1853310f3d52aa434d738d92fd
Hiinnn <hin_pansa@hotmail.com>
2020-03-20 11:08:05 +0700

    gh-pages script

--- src/pages/User-home/User-home.js
+++ src/pages/User-home/User-home.js
@@ -4,15 +4,13 @@ import Slideshow from '../../component/Slideshow/Slideshow';
 import Recommended from '../../component/Recommended/Recommended';
 import './User-home.css';
 
-class UserHome extends React.Component{
+class UserHome extends React.Component {
     render() {
         return (
             <div>
-                <Navbar/>
-                {/* <div className="user-main-content-container"> */}""
-                    <Slideshow/>
-                    <Recommended/>
-                {/* </div> */}
+                <Navbar />
+                <Slideshow />
+                <Recommended />
             </div>
         );
     }
