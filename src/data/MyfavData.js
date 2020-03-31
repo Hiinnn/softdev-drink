ccf2a6cc019d7f3633730effcc403c74d913a9df
AomNatthamon <60309850+AomNatthamon@users.noreply.github.com>
2020-03-31 16:50:24 +0700

    Create MyfavData.js

---
+++ src/data/MyfavData.js
@@ -0,0 +1,29 @@
+export const CafeName = [
+    {
+        index: 1,
+        name: "Cafe's Name 1",
+        openclose: "----time 1----",
+        location: "address 1",
+        type: "type1",
+        contact: "0xx-xxx-xxxx",
+        url: require('../asset/Profile/1.jpg')
+    },
+    {
+        index: 2,
+        name: "Cafe's Name 2",
+        openclose: "----time 2----",
+        location: "address 2",
+        type: "type2",
+        contact: "0xx-xxx-xxxx",
+        url: require('../asset/Profile/1.jpg')
+    },
+    {
+        index: 3,
+        name: "Cafe's Name 3",
+        openclose: "----time 3----",
+        location: "address 3",
+        type: "type3",
+        contact: "0xx-xxx-xxxx",
+        url: require('../asset/Profile/1.jpg')
+    }
+]
